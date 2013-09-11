19fc5a87cd7d0302713f518683342efc1db19962
Peter Salas <psalas@microsoft.com>
2013-09-11 16:05:30 -0700

    Automatically start with root ctreenode when there is only one.

--- webserver/js/boxtree/boxtree.js
+++ webserver/js/boxtree/boxtree.js
@@ -52,6 +52,25 @@ function createBoxTree(pointer, isTreeNode, container) {
     }
 }
 
+function getRootCTreeNodesWithLayoutAssociations() {
+    var roots = [];
+    var docArrayObj = DbgObject.sym("mshtml!g_pts").as("THREADSTATEUI*").deref().f("_paryDoc");
+    var docArray = docArrayObj.f("_pv").as("CDoc*").array(docArrayObj.f("_c").val());
+    for (var i = 0; i < docArray.length; ++i) {
+        var doc = docArray[i];
+        var primaryWindow = doc.f("_pWindowPrimary");
+        if (!primaryWindow.isNull()) {
+            var markup = primaryWindow.f("_pCWindow._pMarkup");
+            var rootTreeNode = markup.f("_ptpFirst").unembed("CTreeNode", "_tpBegin");
+            if (rootTreeNode.f("_fHasLayoutAssociationPtr").val()) {
+                roots.push(rootTreeNode.ptr());
+            }
+        }
+    }
+
+    return roots;
+}
+
 // private:
 
 var boxCache = {};
