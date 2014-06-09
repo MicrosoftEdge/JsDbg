b4bff3813c8f3bad396c700aca74c81a169401ab
Peter Salas <psalas@microsoft.com>
2014-06-09 14:16:46 -0700

    Bump JsDbg version, include layoutbuilder in default extensions.

--- js-dbgeng/Core/WebServer.cs
+++ js-dbgeng/Core/WebServer.cs
@@ -82,7 +82,7 @@ namespace JsDbg {
 
     public class WebServer : IDisposable {
 
-        private const string Version = "2014-01-06-01";
+        private const string Version = "2014-06-09-01";
 
         static public string LocalSupportDirectory
         {
