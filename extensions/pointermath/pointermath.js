6469c0c9e0440f1d8cc1838206d832e981445f32
Peter Salas <psalas@microsoft.com>
2015-05-27 13:20:57 -0700

    Initial PointerMath checkin.

---
+++ extensions/pointermath/pointermath.js
@@ -0,0 +1,12 @@
+"use strict";
+
+var PointerMath = (function() {
+    var maxSize = ((1 << 32) - 1);
+
+    function Pointer(address) {
+    }
+
+    return {
+        Pointer: Pointer
+    };
+})();
