045a756cc8a354c4b91d1d3954805795b49aeb50
Bo Cupp <pcupp@ntdev.microsoft.com>
2015-11-05 16:15:59 -0800

    Revert "Casting treenode to element when _pElement isn't available."
    This reverts commit f6deca9a6dbfba5b477378fa3b9c16a3430981a8.

--- extensions/markuptree/markuptree.js
+++ extensions/markuptree/markuptree.js
@@ -87,10 +87,6 @@ JsDbg.OnLoad(function() {
                 if (etagValue == "ETAG_GENERIC") {
                     // For generic elements, get the tag name/namespace.
                     return treeNode.f("_pElement").vcast()
-                    .then(null, function () {
-                        // The _pElement pointer was removed in RS1.  The treeNode can now be directly cast as an element.
-                        return treeNode.vcast();
-                    })
                     .then(function (element) {
                         return Promise.join([element.f("_cstrTagName._pch").string(), element.f("_cstrNamespace._pch").string(), element.f("_cstrNamespace._pch").isNull()])
                     })
@@ -128,27 +124,16 @@ JsDbg.OnLoad(function() {
 
         DbgObjectTree.AddType(null, MSHTML.Module, "CTreeNode", null, function (object) {
             // Get the subordinate markup.
-            var elementPromise = object.f("_pElement")
-            .then(null, function () {
-                // The _pElement pointer was removed in RS1.  The object can now be directly cast as an element.
-                return object.vcast();
-            });
-
-            var lookasidePromise = elementPromise
-            .then(function (element) {
-                return element.f("_fHasLookasidePtr").val();
-            })
+            var element = object.f("_pElement");
             return Promise.join([
-                elementPromise,
-                lookasidePromise,
+                element.f("_fHasLookasidePtr").val(),
                 DbgObject.constantValue(MSHTML.Module, "CElement::LOOKASIDE", "LOOKASIDE_SUBORDINATE"),
                 DbgObject.constantValue(MSHTML.Module, "CTreeNode", "LOOKASIDE_NODE_NUMBER").then(null, function() { return 0; })
             ])
             .then(function (results) {
-                var element = results[0];
-                var lookasides = results[1];
-                var lookasideSubordinate = results[2];
-                var lookasideNodeNumber = results[3];
+                var lookasides = results[0];
+                var lookasideSubordinate = results[1];
+                var lookasideNodeNumber = results[2];
 
                 if (lookasides & (1 << lookasideSubordinate)) {
                     var hashtable = MSHTML.GetDocFromMarkup(MSHTML.GetMarkupFromElement(element)).f("_HtPvPv");
