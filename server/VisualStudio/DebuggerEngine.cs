4597a0a0114ac358da6f5a36a50a30a0cd51dcfc
Peter Salas <psalas@microsoft.com>
2016-04-18 11:58:50 -0700

    VS JsDbg: Strengthen COM reference handling in DebuggerEngine.

--- server/VisualStudio/DebuggerEngine.cs
+++ server/VisualStudio/DebuggerEngine.cs
@@ -1,12 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
-using JsDbg;
 using Microsoft.VisualStudio.Debugger.Interop;
-using Microsoft.VisualStudio.Shell.Interop;
 using JsDbg.Core;
+using JsDbg.Utilities;
 
 namespace JsDbg.VisualStudio {
     class DebuggerEngine : ITypeCacheDebuggerEngine {
@@ -41,51 +38,51 @@ namespace JsDbg.VisualStudio {
         }
 
         public async Task<Core.SModule> GetModuleForAddress(ulong address) {
-            await this.runner.WaitForBreakIn();
-
-            IEnumDebugModules2 debugModulesEnumerator;
-            if (this.runner.CurrentDebugProgram.EnumModules(out debugModulesEnumerator) == S_OK) {
-                debugModulesEnumerator.Reset();
-                IDebugModule2[] debugModuleArray = new IDebugModule2[1];
-                uint cModules = 0;
-                while (debugModulesEnumerator.Next(1, debugModuleArray, ref cModules) == S_OK && cModules > 0) {
-                    IDebugModule2 debugModule2 = debugModuleArray[0];
-                    MODULE_INFO[] moduleInfo = new MODULE_INFO[1];
-                    if (debugModule2.GetInfo((uint)enum_MODULE_INFO_FIELDS.MIF_NAME | (uint)enum_MODULE_INFO_FIELDS.MIF_LOADADDRESS | (uint)enum_MODULE_INFO_FIELDS.MIF_SIZE, moduleInfo) == S_OK) {
-                        if (moduleInfo[0].m_addrLoadAddress <= address && moduleInfo[0].m_addrLoadAddress + moduleInfo[0].m_dwSize > address) {
-                            string suffixedModuleName = moduleInfo[0].m_bstrName;
-                            string bareModuleName = suffixedModuleName.Substring(0, suffixedModuleName.LastIndexOf('.'));
-                            return new Core.SModule() { BaseAddress = moduleInfo[0].m_addrLoadAddress, Name = bareModuleName };
-                        }
-                    }
-                }
-            }
-
-            throw new DebuggerException(String.Format("Could not find module at address: 0x{x:8}", address));
+            return await this.GetModuleForPredicate(
+                (MODULE_INFO moduleInfo) => {
+                    return moduleInfo.m_addrLoadAddress <= address && moduleInfo.m_addrLoadAddress + moduleInfo.m_dwSize > address;
+                },
+                String.Format("Could not find module at address: 0x{0:x8}", address)
+            );
         }
 
         public async Task<Core.SModule> GetModuleForName(string module) {
+            return await this.GetModuleForPredicate(
+                (MODULE_INFO moduleInfo) => {
+                    return StripModuleSuffix(moduleInfo.m_bstrName) == module;
+                },
+                String.Format("Unknown module: {0}", module)
+            );
+        }
+
+        private static string StripModuleSuffix(string name) {
+            return name.Substring(0, name.LastIndexOf('.'));
+        }
+
+        private async Task<Core.SModule> GetModuleForPredicate(Func<MODULE_INFO, bool> predicate, string errorMessage) {
             await this.runner.WaitForBreakIn();
 
             IEnumDebugModules2 debugModulesEnumerator;
             if (this.runner.CurrentDebugProgram.EnumModules(out debugModulesEnumerator) == S_OK) {
-                debugModulesEnumerator.Reset();
-                IDebugModule2[] debugModuleArray = new IDebugModule2[1];
-                uint cModules = 0;
-                while (debugModulesEnumerator.Next(1, debugModuleArray, ref cModules) == S_OK && cModules > 0) {
-                    IDebugModule2 debugModule2 = debugModuleArray[0];
-                    MODULE_INFO[] moduleInfo = new MODULE_INFO[1];
-                    if (debugModule2.GetInfo((uint)enum_MODULE_INFO_FIELDS.MIF_NAME | (uint)enum_MODULE_INFO_FIELDS.MIF_LOADADDRESS, moduleInfo) == S_OK) {
-                        string suffixedModuleName = moduleInfo[0].m_bstrName;
-                        string bareModuleName = suffixedModuleName.Substring(0, suffixedModuleName.LastIndexOf('.'));
-                        if (bareModuleName == module) {
-                            return new Core.SModule() { BaseAddress = moduleInfo[0].m_addrLoadAddress, Name = module };
+                using (new DisposableComReference(debugModulesEnumerator)) {
+                    debugModulesEnumerator.Reset();
+                    IDebugModule2[] debugModuleArray = new IDebugModule2[1];
+                    uint cModules = 0;
+                    while (debugModulesEnumerator.Next(1, debugModuleArray, ref cModules) == S_OK && cModules > 0) {
+                        IDebugModule2 debugModule2 = debugModuleArray[0];
+                        using (new DisposableComReference(debugModule2)) {
+                            MODULE_INFO[] moduleInfo = new MODULE_INFO[1];
+                            if (debugModule2.GetInfo((uint)enum_MODULE_INFO_FIELDS.MIF_NAME | (uint)enum_MODULE_INFO_FIELDS.MIF_LOADADDRESS | (uint)enum_MODULE_INFO_FIELDS.MIF_SIZE, moduleInfo) == S_OK) {
+                                if (predicate(moduleInfo[0])) {
+                                    return new Core.SModule() { BaseAddress = moduleInfo[0].m_addrLoadAddress, Name = StripModuleSuffix(moduleInfo[0].m_bstrName) };
+                                }
+                            }
                         }
                     }
                 }
             }
 
-            throw new DebuggerException(String.Format("Unknown module: {0}", module));
+            throw new DebuggerException(errorMessage);
         }
 
         private async Task<byte[]> ReadByteArray(ulong pointer, ulong size) {
@@ -94,14 +91,16 @@ namespace JsDbg.VisualStudio {
             IDebugMemoryContext2 memoryContextTarget;
             this.runner.CurrentMemoryContext.Add(pointer, out memoryContextTarget);
 
-            uint dwRead;
-            uint dwUnReadable = 0;
-            byte[] memory = new byte[size];
-            this.runner.CurrentMemoryBytes.ReadAt(memoryContextTarget, (uint)size, memory, out dwRead, ref dwUnReadable);
-            if (dwRead != size) {
-                throw new DebuggerException(String.Format("ReadArray: Failed read memory 0x{0:x8} - Size {0}", pointer, size));
+            using (new DisposableComReference(memoryContextTarget)) {
+                uint dwRead;
+                uint dwUnReadable = 0;
+                byte[] memory = new byte[size];
+                this.runner.CurrentMemoryBytes.ReadAt(memoryContextTarget, (uint)size, memory, out dwRead, ref dwUnReadable);
+                if (dwRead != size) {
+                    throw new DebuggerException(String.Format("ReadArray: Failed read memory 0x{0:x8} - Size {0}", pointer, size));
+                }
+                return memory;
             }
-            return memory;
         }
 
         public async Task<T[]> ReadArray<T>(ulong pointer, ulong size) where T : struct {
@@ -138,27 +137,30 @@ namespace JsDbg.VisualStudio {
             IEnumDebugFrameInfo2 frameEnumerator;
             thread.EnumFrameInfo((uint)(enum_FRAMEINFO_FLAGS.FIF_FRAME | enum_FRAMEINFO_FLAGS.FIF_STACKRANGE), 10, out frameEnumerator);
 
-            uint frameCount = 0;
-            frameEnumerator.GetCount(out frameCount);
-            FRAMEINFO[] frames = new FRAMEINFO[frameCount];
+            using (new DisposableComReference(frameEnumerator)) {
+                uint frameCount = 0;
+                frameEnumerator.GetCount(out frameCount);
+                FRAMEINFO[] frames = new FRAMEINFO[frameCount];
 
-            frameEnumerator.Reset();
-            if (frameEnumerator.Next((uint)frames.Length, frames, ref frameCount) == S_OK) {
-                for (int i = 0; i < frameCount; ++i) {
-                    FRAMEINFO frame = frames[i];
-
-                    IDebugCodeContext2 codeContext;
-                    if (frame.m_pFrame.GetCodeContext(out codeContext) == S_OK) {
-                        CONTEXT_INFO[] contextInfo = new CONTEXT_INFO[1];
-                        codeContext.GetInfo((uint)enum_CONTEXT_INFO_FIELDS.CIF_ADDRESS, contextInfo);
-                        ulong instructionAddress = ulong.Parse(contextInfo[0].bstrAddress.Substring(2), System.Globalization.NumberStyles.AllowHexSpecifier);
-                        // TODO: the -8 below seems architecture dependent
-                        results.Add(new Core.SStackFrameWithContext() { Context = null, StackFrame = new SStackFrame() { FrameAddress = frame.m_addrMin - 8, InstructionAddress = instructionAddress, StackAddress = frame.m_addrMax } });
+                frameEnumerator.Reset();
+                if (frameEnumerator.Next((uint)frames.Length, frames, ref frameCount) == S_OK) {
+                    for (int i = 0; i < frameCount; ++i) {
+                        FRAMEINFO frame = frames[i];
+                        IDebugCodeContext2 codeContext;
+                        if (frame.m_pFrame.GetCodeContext(out codeContext) == S_OK) {
+                            using (new DisposableComReference(codeContext)) {
+                                CONTEXT_INFO[] contextInfo = new CONTEXT_INFO[1];
+                                codeContext.GetInfo((uint)enum_CONTEXT_INFO_FIELDS.CIF_ADDRESS, contextInfo);
+                                ulong instructionAddress = ulong.Parse(contextInfo[0].bstrAddress.Substring(2), System.Globalization.NumberStyles.AllowHexSpecifier);
+                                // TODO: the -8 below seems architecture dependent
+                                results.Add(new Core.SStackFrameWithContext() { Context = null, StackFrame = new SStackFrame() { FrameAddress = frame.m_addrMin - 8, InstructionAddress = instructionAddress, StackAddress = frame.m_addrMax } });
+                            }
+                        }
                     }
                 }
-            }
 
-            return results;
+                return results;
+            }
         }
 
         public event DebuggerChangeEventHandler DebuggerChange;
