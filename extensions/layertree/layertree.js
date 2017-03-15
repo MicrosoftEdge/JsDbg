7b088ec01fef177ba91a55d4600481934ba938fb
Peter Salas <psalas@microsoft.com>
2017-03-15 16:13:21 -0700

    LayerTree: Add object actions and vcast to final types.

--- extensions/layertree/layertree.js
+++ extensions/layertree/layertree.js
@@ -12,20 +12,20 @@ Loader.OnLoad(function() {
         GetRoots: function() {
             return MSHTML.GetCDocs()
             .filter(function (doc) {
-                return doc.f("_view._pDispRoot").isNull()
+                return doc.f("_spLayerManager.m_pT").isNull()
                 .then(function (isNull) {
                     return !isNull;
                 })
             })
-            .then(function(docsWithDispRoots) {
-                if (docsWithDispRoots.length == 0) {
+            .then(function(docsWithLayerManagers) {
+                if (docsWithLayerManagers.length == 0) {
                     return Promise.reject();
                 }
-                return docsWithDispRoots;
+                return docsWithLayerManagers;
             })
             .then(null, function(error) {
                 var errorMessage =
-                    "No CDispRoots were found.\
+                    "No Layer Managers were found.\
                     Possible reasons:\
                     <ul>\
                         <li>The debuggee is not IE 11 or Edge.</li>\
@@ -33,7 +33,7 @@ Loader.OnLoad(function() {
                         <li>The debugger is in 64-bit mode on a WoW64 process (\".effmach x86\" will fix).</li>\
                         <li>Symbols aren't available.</li>\
                     </ul>\
-                    Refresh the page to try again, or specify a CDispNode explicitly.";
+                    Refresh the page to try again, or specify an object explicitly.";
 
                 if (error) {
                     errorMessage = "<h4>" + error.toString() + "</h4>" + errorMessage;
@@ -59,92 +59,53 @@ Loader.OnLoad(function() {
         })
     })
 
+    LayerTree.Renderer.addNameRenderer(MSHTML.Module, function (x) { return (x.indexOf("RefCounted") == 0); }, function (refCounted) {
+        var typeString = refCounted.htmlTypeDescription();
+        typeString = typeString.substr(typeString.indexOf("&lt;") + 4);
+        typeString = typeString.substr(0, typeString.lastIndexOf(","));
+        return typeString;
+    })
+
     LayerTree.Tree.addChildren(MSHTML.Module, "CWUCLayerManager", function (layerManager) {
         return layerManager.f("_spRootLayer.m_pT").vcast();
     })
 
     LayerTree.Tree.addChildren(MSHTML.Module, "CWUCLayer", function (layerManager) {
-        return layerManager.f("_pFirstChildLayer").list("_pNextLayer");
+        return layerManager.f("_pFirstChildLayer").list("_pNextLayer").vcast();
     })
-    /*
+
+    function getDocOrLayerManager(layerManager) {
+        // Check if there's a CDoc that owns it.
+        return MSHTML.GetCDocs()
+        .filter(function (doc) {
+            return doc.f("_spLayerManager.m_pT").vcast().equals(layerManager)
+        })
+        .then(function (docs) {
+            if (docs.length > 0) {
+                return docs[0];
+            } else {
+                return layerManager;
+            }
+        })
+    }
 
     // Add DbgObject actions for links to the display tree.
-    DbgObject.AddAction(MSHTML.Module, "CDispNode", "DisplayTree", function (dispNode) {
-        function getTopMostDispNode(node) {
-            return node.f("_pParent")
-            .then(function (parentNode) {
-                if (parentNode.isNull()) {
-                    return node;
-                } else {
-                    return getTopMostDispNode(parentNode);
-                }
-            })
-        }
-
-        return getTopMostDispNode(dispNode)
-        .then(function (topMostDispNode) {
-            // Check if there's a CDoc that owns it.
-            return MSHTML.GetCDocs()
-            .filter(function (doc) {
-                return doc.f("_view._pDispRoot").equals(topMostDispNode)
-            })
-            .then(function (docs) {
-                if (docs.length > 0) {
-                    return docs[0];
-                } else {
-                    return topMostDispNode;
-                }
-            })
-        })
+    DbgObject.AddAction(MSHTML.Module, "IDispLayer", "LayerTree", function (layer) {
+        return layer.vcast().f("_pLayerManager").vcast()
+        .then(getDocOrLayerManager)
         .then(function (rootNode) {
-            return TreeInspector.GetActions("displaytree", "Display Tree", rootNode, dispNode);
+            return TreeInspector.GetActions("layertree", "Layer Tree", rootNode, layer);
         })
     });
 
-    DbgObject.AddAction(MSHTML.Module, "CDoc", "DisplayTree", function (doc) {
-        return TreeInspector.GetActions("displaytree", "Display Tree", doc);
-    });
-
-    DbgObject.AddAction(MSHTML.Module, "CView", "DisplayTree", function (view) {
-        return TreeInspector.GetActions("displaytree", "Display Tree", view.unembed("CDoc", "_view"), view);
-    });
-
-    DbgObject.AddExtendedField(MSHTML.Module, "CDispNode", "Client", "CDispClient", UserEditableFunctions.Create(function (dispNode) {
-        // Check if it has advanced display...
-        return dispNode.f("_flags._fAdvanced").val()
-
-        // And get the disp client.
-        .then(
-            function(hasAdvanced) {
-                if (hasAdvanced) {
-                    return dispNode.f("_pAdvancedDisplay._pDispClient").vcast();
-                } else {
-                    return dispNode.f("_pDispClient").vcast();
-                }
-            },
-            function() {
-                return dispNode.f("_pDispClient").vcast();
-            }
-        );
-    }));
-
-    DbgObject.AddExtendedField(MSHTML.Module, "CDispClient", "AsContainerBox", "Layout::ContainerBox", UserEditableFunctions.Create(function (client) {
-        return client.dcast("Layout::ContainerBox");
-    }));
-
-    DbgObject.AddTypeDescription(MSHTML.Module, "CDispFlags", "AllFlags", false, UserEditableFunctions.Create(function (flags) {
-        return Promise
-        .filter(flags.fields(), function(f) {
-            if (f.name.indexOf("_fUnused") != 0 && f.value.bitcount == 1) {
-                return f.value.val();
-            } else {
-                return false;
-            }
+    DbgObject.AddAction(MSHTML.Module, "IDispLayerManager", "LayerTree", function (layerManager) {
+        return getDocOrLayerManager(layerManager)
+        .then(function (rootNode) {
+            return TreeInspector.GetActions("layertree", "Layer Tree", rootNode, layerManager);
         })
-        .then(function(enabledFlags) {
-            return enabledFlags
-            .map(function(flag) { return flag.name; })
-            .join(" ");
-        });
-    }));*/
+    });
+
+    DbgObject.AddAction(MSHTML.Module, "CDoc", "LayerTree", function (doc) {
+        return TreeInspector.GetActions("layertree", "Layer Tree", doc);
+    });
 });
