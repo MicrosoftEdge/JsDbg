3642b494ab8aa0397cd1cda1c969aaa74fc4b086
Sanket Joshi (EDGE) <sajos@microsoft.com>
2018-11-05 15:11:14 -0800

    ChromiumHelpers: Treat all cc related modules as equivalent.

--- extensions/target-specific/chromium/chromium-helpers/chromium-helpers.js
+++ extensions/target-specific/chromium/chromium-helpers/chromium-helpers.js
@@ -50,8 +50,9 @@ var Chromium = null;
             _help_RendererProcessEquivalentModules: {
                 description: "List of modules used in the Chromium renderer process.",
             },
-            RendererProcessEquivalentModules: ["blink_common", "blink_core", "blink_platform", "blink_modules", "blink_android_mojo_bindings_shared",
-                "blink_embedded_frame_sink_mojo_bindings_shared", "blink_core_mojo_bindings_shared", "blink_controller", "cc", "chrome_child"],
+            RendererProcessEquivalentModules: ["chrome_child", "blink_core", "blink_common", "blink_platform", "blink_modules", "blink_android_mojo_bindings_shared",
+                "blink_embedded_frame_sink_mojo_bindings_shared", "blink_core_mojo_bindings_shared", "blink_controller", "cc", "cc_animation", "cc_paint",
+                "cc_base", "cc_debug", "cc_mojo_embedder"],
 
             _help_BrowserProcessEquivalentModules: {
                 description: "List of modules used in the Chromium browser process.",
