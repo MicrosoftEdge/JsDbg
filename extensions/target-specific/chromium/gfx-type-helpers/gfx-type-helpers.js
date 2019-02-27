31447130cc10d20d168cee775607c175dc140f93
Sanket Joshi <sajos@microsoft.com>
2019-02-27 15:37:42 -0800

    GfxHelpers: Add default descriptions for PointF, Point3F, Vector2dF and ScrollOffset.

--- extensions/target-specific/chromium/gfx-type-helpers/gfx-type-helpers.js
+++ extensions/target-specific/chromium/gfx-type-helpers/gfx-type-helpers.js
@@ -11,7 +11,39 @@ Loader.OnLoad(function() {
         Chromium.RendererProcessType("gfx::Point"),
         "Point",
         true,
-        UserEditableFunctions.Create((size) => Promise.all([size.f("x_").val(), size.f("y_").val()])
+        UserEditableFunctions.Create((point) => Promise.all([point.f("x_").val(), point.f("y_").val()])
+            .thenAll((first, second) => `{${first}, ${second}}`))
+    );
+
+    DbgObject.AddTypeDescription(
+        Chromium.RendererProcessType("gfx::PointF"),
+        "Point",
+        true,
+        UserEditableFunctions.Create((point) => Promise.all([point.f("x_").val(), point.f("y_").val()])
+            .thenAll((first, second) => `{${first}, ${second}}`))
+    );
+
+    DbgObject.AddTypeDescription(
+        Chromium.RendererProcessType("gfx::Point3F"),
+        "Point",
+        true,
+        UserEditableFunctions.Create((point) => Promise.all([point.f("x_").val(), point.f("y_").val(), point.f("z_").val()])
+            .thenAll((first, second, third) => `{${first}, ${second}, ${third}}`))
+    );
+
+    DbgObject.AddTypeDescription(
+        Chromium.RendererProcessType("gfx::Vector2dF"),
+        "Delta",
+        true,
+        UserEditableFunctions.Create((vector) => Promise.all([vector.f("x_").val(), vector.f("y_").val()])
+            .thenAll((first, second) => `{${first}, ${second}}`))
+    );
+
+    DbgObject.AddTypeDescription(
+        Chromium.RendererProcessType("gfx::ScrollOffset"),
+        "Offset",
+        true,
+        UserEditableFunctions.Create((scrollOffset) => Promise.all([scrollOffset.f("x_").val(), scrollOffset.f("y_").val()])
             .thenAll((first, second) => `{${first}, ${second}}`))
     );
 })
