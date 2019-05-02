c0cef2e24b673015638cf37543cc9e6b2f353373
Christian Biesinger <cbiesinger@chromium.org>
2019-05-02 12:35:52 -0500

    Make FileCache work on non-Windows platforms

---
+++ server/JsDbg.Core/DllImports.cs
@@ -0,0 +1,19 @@
+﻿//--------------------------------------------------------------
+//
+//    MIT License
+//
+//    Copyright (c) Microsoft Corporation. All rights reserved.
+//
+//--------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.InteropServices;
+
+namespace JsDbg.Core {
+    internal class DllImports {
+        [DllImport("shlwapi.dll")]
+        internal static extern bool PathIsNetworkPath(string path);
+    }
+}
