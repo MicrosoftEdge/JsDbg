a3ce17d4a6d04a8871cec9b9004058a9795fb4fa
Christian Biesinger <cbiesinger@chromium.org>
2019-07-08 12:15:25 -0500

    [gdb] Delete create_package.sh
    It is redundant with "make package"

--- server/JsDbg.Gdb/create_package.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-set -ex
-~/dotnet/dotnet publish -c Release -r linux-x64
-cd bin/Release/netcoreapp2.1/linux-x64
-tar --transform='s/publish/jsdbg/' -c -j -f ../../../../jsdbg.tar.bz2 publish/
