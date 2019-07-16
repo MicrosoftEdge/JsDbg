72330b1cca7cf9c23298b03a9dca03d66d8f61fc
Christian Biesinger <cbiesinger@chromium.org>
2019-07-16 16:07:01 -0500

    [gdb] Add an integration test
    This tests that GDB successfully loads the python script and that its
    various functions work correctly on a test program.
    
    Uses the "dejagnu" tool, which has poor documentation but
    seems otherwise well suited to testing command-line interfaces
    like this one (gdb uses it for its tests too).

---
+++ server/JsDbg.Gdb/testsuite/test_program.cc
@@ -0,0 +1,21 @@
+int global_var = 42;
+
+class Base {
+  int base_member_;
+};
+
+class Class : public Base {
+  struct {
+    int member_;
+  };
+};
+
+enum class Enum {
+  EFirst = 1
+};
+
+int main() {
+  Class c;
+  Enum e;
+  return global_var;
+}
